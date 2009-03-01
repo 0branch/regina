48a8f2dea6e062a7b4e44d0c9cb40f2477ded093
rexx <rexx@528c8479-552a-0410-8426-e933f26d916d>
2009-03-01 08:57:30 +0000

    Increase variable hash table to 2003
    git-svn-id: https://svn.code.sf.net/p/regina-rexx/code/interpreter/trunk@57 528c8479-552a-0410-8426-e933f26d916d

--- variable.c
+++ variable.c
@@ -1147,7 +1147,7 @@ int init_vars( tsd_t *TSD )
       return 0;
    vt = (var_tsd_t *)TSD->var_tsd;
    memset( vt, 0, sizeof( var_tsd_t ) );
-   vt->initialHashTableLength = 503;
+   vt->initialHashTableLength = 2003;
 
 #ifdef DEBUG
    {
