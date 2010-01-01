a0486592525bf19edd6efd937a1dd6065e8e84be
rexx <rexx@528c8479-552a-0410-8426-e933f26d916d>
2010-01-01 02:11:14 +0000

    Remove RCS id
    git-svn-id: https://svn.code.sf.net/p/regina-rexx/code/interpreter/trunk@75 528c8479-552a-0410-8426-e933f26d916d

--- common/rxpack.c
+++ common/rxpack.c
@@ -15,9 +15,6 @@
  * License along with this library; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
-static char RCSid[] = "$Id: rxpack.c,v 1.55 2006/07/10 07:13:17 mark Exp $";
-
 #if defined(OS2) || defined(__OS2__)
 # define INCL_DOSMISC
 #endif
